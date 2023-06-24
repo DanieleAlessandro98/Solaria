e70813217066671dad743374b7c9ea3cb67c0785
Achilleee <60854105+DanieleAlessandro98@users.noreply.github.com>
2023-06-24 13:56:21 +0200

    Implemented first LevelManager prototype

---
+++ Assets/Script/LevelManager.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    private static LevelManager m_Singleton;
+
+    private int m_CurrentLevel;
+
+    public static LevelManager Singleton
+    {
+        get
+        {
+            return m_Singleton;
+        }
+    }
+
+    void Awake()
+    {
+        if (m_Singleton)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        m_Singleton = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_CurrentLevel = 0; //TODO: implementare sistema salvataggio livello
+        StartCoroutine(WaitForShowLevel());
+    }
+
+    private IEnumerator WaitForShowLevel()
+    {
+        yield return new WaitForEndOfFrame();
+
+        ShowLevel();
+    }
+
+    private void ShowLevel()
+    {
+        throw new NotImplementedException("things to display level information to user. Level: " + m_CurrentLevel);
+    }
+}
